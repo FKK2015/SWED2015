5c789cd18a2a57e0e13b4038eca8297734fa924e
Kris <Kris@10.53.223.32>
2015-06-08 17:11:16 +0200

    new commit 8.06

--- src/Simulator/State.java
+++ src/Simulator/State.java
@@ -10,5 +10,32 @@ package Simulator;
  * @author Kris
  */
 class State {
+    private boolean doorState = false;
+    private boolean bellState = false;
+    private boolean floor1ButtonState = false;
+    private boolean floor2ButtonState= false;
+    private boolean floor1Content = false;
+    private boolean floor2Content = false;
+    private boolean liftContent = false;
+    private int liftPos = 0;
+    private boolean liftButtonTo1State = false;
+    private boolean liftButtonTo2State = false;
+    private int currentTime = 0;
     
+    public void print(){
+        
+    }
+    public void changeDoorState(){
+        doorState ^= true;
+    }
+    public void changeBellState(){
+        bellState ^= true;
+    }
+    public void changeF1buttonState(){
+        floor1ButtonState ^= true;
+    }
+    public void changeF2buttonState(){
+        floor2ButtonState ^= true;
+    }     
+    public      
 }
